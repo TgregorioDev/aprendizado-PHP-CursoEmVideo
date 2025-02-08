57442ff0d27c4489f0c44d7bc613fec11a23fbc7
Tiago Gregório <162623049+TgregorioDev@users.noreply.github.com>
2025-02-08 13:06:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplos das 7 regras</title>
+    <title>Variáveis e Constantes - PHP</title>
 </head>
 <body>
-    <h1>Exemplos das 7 regras</h1>
-        <?php 
-            $nome = "Tiago";
-            $salario = 2500.75;
+    <h1>Variáveis e Constantes - PHP</h1>
 
-            //echo "É verdade que seu nome é $NOME ?"; -> este código da erro devida a 6° regra do case sensitive
-            echo "É verdade que seu nome é $nome? ";
-            echo "Seu salário é $salario ";
+    <?php 
+        $nome = "Tiago"; //definição de variável com o '$' + nome da variável = valor da variável;
+        $sobrenome = "Gregório";
+        //
+        //
+        //
+        $sobrenome = "Teles"; //caso declare outra variável com mesmo nome, porém outro valor, o valor a ser apresentado na mensagem será o último valor declarado, pois é uma VARIÁVEL
+        const PAIS = "Brasil"; //o nome da constante não é declarado com o '$' 
+        //
+        //
+        //const PAIS = "EUA"; (ao executar com essa constante, deu erro, pois já foi definida anteriormente e o valor de uma constante NÃO SE ALTERA)
 
-        ?>
+        echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS; //echo escreve mensagem na tela, e você traz as variáveis para que seus valores completem a mensagem;
+                                                                     //para chamar a constante na frase, basta após as aspas escrever . NOME DA CONSTANTE;
+    ?>
 </body>
 </html>
